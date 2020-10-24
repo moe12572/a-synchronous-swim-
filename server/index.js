671907fcab689e38d29dadc3959162bf41054ec7
Fabian Yee <fabianyee952@gmail.com>
2020-10-23 18:07:07 -0700

    progress

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 
 
+
 const keypressHandler = require('./js/keypressHandler');
 
 keypressHandler.initialize(message => console.log(`Message received: ${message}`));
@@ -8,10 +9,12 @@ const httpHandler = require('./js/httpHandler');
 
 
 const http = require('http');
+debugger;
 const server = http.createServer(httpHandler.router);
 
 const port = 3000;
 const ip = '127.0.0.1';
+
 server.listen(port, ip);
 
 console.log('Server is running in the terminal!');
